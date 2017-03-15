d6a5ec505a5e9703536fb783ffce258d2bdffd1f
Chelsey Eichler <chelsey.eichler@yahoo.com>
2017-03-14 21:21:20 -0400

    updated my files so that there are no longer any errors and added in ngRoute

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-var app = angular.module('myApp',['ngAnimate']);
+var app = angular.module('myApp',['ngRoute']);
 
-.controller('myController', function($scope){
+app.controller('myController', function($scope){
   $scope.words = [
-    {word:'yourName', placeholder:'first name'}
-    {word:'animal', placeholder:'animal'}
-    {word:'foodDish', placeholder:'fooddish'}
+    {word:'yourName', placeholder:'first name'},
+    {word:'animal', placeholder:'animal'},
+    {word:'foodDish', placeholder:'fooddish'},
+    {word:'verb', placeholder:'verb'},
+    {word:'color', placeholder:'color'},
+    {word:'object', placeholder:'object'},
+    {word:'place', placeholder:'place'}
   ]
 
+})
 
+
+app.config(function($routeProvider, $locationProvider){
+
+  $routeProvider
+    .when('/userInput', {
+      controller: 'inputCtrl',
+      templateUrl: 'views/searchpage.html'
+    })
+  .when('/viewsubreddit', {
+    controller: 'viewCtrl',
+    templateUrl: 'views/displaypage.html'
+  });
+
+  $locationProvider.hashPrefix('');
 
 })
